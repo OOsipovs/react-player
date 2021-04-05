a1b52550c5cc1045116eb8a7c60061cc902498b3
Olegs <olegs.osipovs@gmail.com>
2021-04-05 15:47:06 +0300

    Playing fnctionality implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/app.scss';
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './util';
 
 function App() {
+
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong={currentSong}/>
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
 }
